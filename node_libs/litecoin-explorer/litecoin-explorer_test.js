fcea193c87a19ed439ec7146a1de96e1e6634ac3
Thomas Dall'Agnese <thomas@dallagnese.fr>
2014-03-21 12:42:39 +0900

    Update litecoin-explorer_test.js

--- node_libs/litecoin-explorer/litecoin-explorer_test.js
+++ node_libs/litecoin-explorer/litecoin-explorer_test.js
@@ -4,7 +4,3 @@ var w = 'wallet-address';
 ltc.getBalance(w, function(response){
     console.log(response);
 });
-w='LVUKAoJjBTqR3UMQYrn2DZdSfWXSwLunDM';
-ltc.getBalance(w, function(response){
-    console.log(response);
-});
